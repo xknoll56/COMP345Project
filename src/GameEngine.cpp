d5ad2e1d5021d7c598aed1e016193b36f151ca7b
sophoreno <47183829+sophoreno@users.noreply.github.com>
2020-11-17 19:22:10 -0500

    Continued Part 1 + added Destructor

--- src/GameEngine.cpp
+++ src/GameEngine.cpp
@@ -9,13 +9,14 @@
 
 // Part 1: Game Start.
 void GameEngine::Init() {
-  // 1. Select a map and load it.
+	/* 1. Select a map and load it. */
 	std::string map_name;
 	std::string file_name;
 	MapLoader* ml = new MapLoader();
 	std::cout << "Here are the available maps:\n---------------------------\n"
 		<< "artic.map\nbigeurope.map\ncanada.map\neurope.map\nfrance.map\nsolar.map\nswiss.map\n";
-	
+
+	// Loops until the user enters a valid map
 	do {
 		std::cout << "\nEnter your choice of map: ";
 		std::cin >> map_name;
@@ -26,10 +27,13 @@ void GameEngine::Init() {
 			std::cout << "Please try again." << std::endl;
 	} while (map == nullptr);
 	
+	// Deleting the map loader
 	delete ml;
 	
-  // 2. Select num of Players.
+	/* 2. Select number of Players. */
 	int num_players;
+
+	// Loops until the user enters a valid number of players
 	do {
 		std::cout << "\nEnter the number of players (2-5) : ";
 		std::cin >> num_players;
@@ -37,13 +41,16 @@ void GameEngine::Init() {
 		if (num_players < 2 || num_players > 5)
 			std::cout << "Incorrect number of players, try again." << std::endl;
 	} while (num_players < 2 || num_players > 5);
-    players.reserve(num_players);
 
-  // 3. Set Observers (Part 5).
-	// doesnt actually turn them on or off yet, it just gathers whether the player wants them on or off.
+    // Allocating the right number of space in the vector
+	players.reserve(num_players);
+
+	/* 3. Set Observers (Part 5). */
 	bool setObs = false;
 	char answer;
 	char answerCap;
+
+	// Loops until the user enters an appropriate answer
 	do {
 		std::cout << "\nDo you want to enable observers for the game? (Y/N) ";
 		std::cin >> answer;
@@ -52,30 +59,49 @@ void GameEngine::Init() {
 			std::cout << "Incorrect response, try again." << std::endl;
 	} while (answerCap != 'Y' && answerCap != 'N');
 	
-	if (answerCap == 'Y')
+	if (answerCap == 'Y') 
 		setObs = true;
+		
+	// Initializing the Observers
+	gameStatsObs = new GameStatisticsObserver(map->GetTerritories()->size());
+	phaseObs = new PhaseObserver();
 
-	std::cout << "\nObservers are now set to " << setObs << std::endl;
+	// Displaying the observer information to the user
+	std::cout << "\nObservers are now " << (setObs ? "enabled." : "disabled.") << std::endl;
 
-  // 4. Create Players.
+	/* 4. Create Players. */
 	for (int i = 0; i < num_players; i++) {
 		players.push_back(new Player(map));
-		std::cout << "Player " << i + 1 << " has been created." <<std::endl;
+		std::cout << "Player " << i + 1 << " has been created with an empty hand of cards. " << std::endl;
+	} 
+
+	// We only attach the observers and the players if we want them turned on
+	if (setObs) {
+		for (Player* p : players) {
+			// Attaching every player to both observers
+			gameStatsObs->AddPlayer(p); 
+			phaseObs->AddPlayer(p);
+			// Attaching both observers to every player
+			p->Attach(gameStatsObs);
+			p->Attach(phaseObs);
+		}
 	}
 		
-  // 5. Create a deck of cards; 
-  deck.addCard(new BombCard{10});
-  deck.addCard(new ReinforcementCard{20});
-  deck.addCard(new BlockadeCard{30});
-  deck.addCard(new AirliftCard{40});
-  deck.addCard(new DiplomacyCard{50});
-  deck.addCard(new BombCard{60});
-  deck.addCard(new ReinforcementCard{70});
-  deck.addCard(new BlockadeCard{80});
-  deck.addCard(new AirliftCard{90});
-  deck.addCard(new DiplomacyCard{100});
+	/* 5. Create a deck of cards. */ 
+	deck.addCard(new BombCard{10});
+	deck.addCard(new ReinforcementCard{20});
+	deck.addCard(new BlockadeCard{30});
+	deck.addCard(new AirliftCard{40});
+	deck.addCard(new DiplomacyCard{50});
+	deck.addCard(new BombCard{60});
+	deck.addCard(new ReinforcementCard{70});
+	deck.addCard(new BlockadeCard{80});
+	deck.addCard(new AirliftCard{90});
+	deck.addCard(new DiplomacyCard{100});
 
-  // show that the deck has been created with the right number of cards 
+	// Displaying the deck information to the user
+	std::cout << "\nA deck of 10 cards has been created. " << std::endl; // change 10 to deck.size of something like that
+
 }
 
 // Part 2: Game Startup
@@ -171,3 +197,19 @@ bool GameEngine::ExecuteOrdersPhase() {
 Player* GameEngine::neutralPlayer = new Player();
 
 Player* GameEngine::GetNeutralPlayer() { return neutralPlayer; }
+
+// Destructor
+GameEngine::~GameEngine() {
+	for (Player* p : players) {
+		delete p;
+		p = nullptr;
+	}
+	delete gameStatsObs;
+	gameStatsObs = nullptr;
+	delete phaseObs;
+	phaseObs = nullptr;
+	delete neutralPlayer;
+	neutralPlayer = nullptr; 
+	delete map;
+	map = nullptr;
+}
